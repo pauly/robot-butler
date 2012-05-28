b1604a1495b645974137e93cfea012006bd5f62b
Paul Clarke <pauly@clarkeology.com>
2012-05-28 13:40:05 +0100

    confirmation response on clicking buttons

---
+++ mock.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Mock server for the robot butler
+ * 
+ * @author PC <pauly@clarkeology.com>
+ * @date Sun May 27 17:55:35 BST 2012
+ */
+
+header( 'content-type: application/json' );
+echo json_encode( array( 'a' => array( '3' => 666 ), 'd' => array( '13' => 1 ), 'r' => array( 'method' => $_SERVER['REQUEST_METHOD'], 'path' => $_SERVER['REQUEST_URI'], 'pin' => '13', 'msg' => 'Thanks!' )));
+
+?>
